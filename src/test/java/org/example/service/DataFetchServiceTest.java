4ba7032ccc239398975697d0dbd7ba82f1354a7a
Kiss Adrián Imre <kisswadrian857@gmail.com>
2024-02-28 21:23:41 +0100

    Adds tests for DataFetchService class

--- src/test/java/org/example/service/DataFetchServiceTest.java
+++ src/test/java/org/example/service/DataFetchServiceTest.java
@@ -5,7 +5,6 @@ import org.example.dto.SurveyStatistics;
 import org.example.model.Member;
 import org.example.model.Participation;
 import org.example.model.Survey;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -16,6 +15,11 @@ import java.util.stream.Stream;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class DataFetchServiceTest {
+
+    private static final Integer NOT_ASKED = 1;
+    private static final Integer REJECTED = 2;
+    private static final Integer FILTERED = 3;
+    private static final Integer COMPLETED = 4;
 
     private DataFetchService underTest;
 
@@ -34,8 +38,31 @@ class DataFetchServiceTest {
     }
 
     private static Stream<Arguments> fetchRespondersBySurveyIdSource() {
+        Member member1 = Member.builder().memberId(1).fullName("Malissa Arn").build();
+        Member member2 = Member.builder().memberId(2).fullName("Teri Villalobos").build();
+
         return Stream.of(
-                Arguments.of()
+                Arguments.of(
+                        List.of(),
+                        List.of(member1),
+                        1,
+                        List.of()
+                ),
+                Arguments.of(
+                        List.of(Participation.builder().surveyId(2).memberId(2).status(COMPLETED).build(),
+                                Participation.builder().surveyId(2).memberId(1).status(NOT_ASKED).build(),
+                                Participation.builder().surveyId(3).memberId(2).status(COMPLETED).build()),
+                        List.of(member1, member2),
+                        2,
+                        List.of(member2)
+                ),
+                Arguments.of(
+                        List.of(Participation.builder().surveyId(2).memberId(2).status(COMPLETED).build(),
+                                Participation.builder().surveyId(2).memberId(1).status(COMPLETED).build()),
+                        List.of(member1, member2),
+                        2,
+                        List.of(member1, member2)
+                )
         );
     }
 
@@ -54,8 +81,30 @@ class DataFetchServiceTest {
     }
 
     private static Stream<Arguments> fetchSurveyByMemberIdSource() {
+        Survey survey1 = Survey.builder().surveyId(1).name("Survey1").build();
+        Survey survey2 = Survey.builder().surveyId(2).name("Survey2").build();
+
         return Stream.of(
-                Arguments.of()
+                Arguments.of(
+                        List.of(),
+                        List.of(survey1),
+                        2,
+                        List.of()
+                ),
+                Arguments.of(
+                        List.of(Participation.builder().memberId(2).surveyId(1).status(COMPLETED).build(),
+                                Participation.builder().memberId(2).surveyId(2).status(COMPLETED).build()),
+                        List.of(survey1, survey2),
+                        2,
+                        List.of(survey1, survey2)
+                ),
+                Arguments.of(
+                        List.of(Participation.builder().memberId(2).surveyId(1).status(REJECTED).build(),
+                                Participation.builder().memberId(2).surveyId(2).status(COMPLETED).build()),
+                        List.of(survey1, survey2),
+                        2,
+                        List.of(survey2)
+                )
         );
     }
 
@@ -74,17 +123,40 @@ class DataFetchServiceTest {
     }
 
     private static Stream<Arguments> fetchPointsByMemberIdSource() {
+        Survey survey1 = Survey.builder().surveyId(1).filteredPoints(3).completionPoints(5).build();
+        Survey survey2 = Survey.builder().surveyId(2).filteredPoints(1).completionPoints(10).build();
+
         return Stream.of(
-                Arguments.of()
+                Arguments.of(
+                        List.of(Participation.builder().memberId(1).surveyId(1).status(COMPLETED).build(),
+                                Participation.builder().memberId(1).surveyId(2).status(FILTERED).build()),
+                        List.of(survey1, survey2),
+                        1,
+                        List.of(SurveyPoint.builder().surveyId(1).points(5).build(),
+                                SurveyPoint.builder().surveyId(2).points(1).build())
+                ),
+                Arguments.of(
+                        List.of(),
+                        List.of(survey1, survey2),
+                        1,
+                        List.of()
+                ),
+                Arguments.of(
+                        List.of(Participation.builder().memberId(1).surveyId(1).status(NOT_ASKED).build(),
+                                Participation.builder().memberId(1).surveyId(2).status(FILTERED).build()),
+                        List.of(survey1, survey2),
+                        1,
+                        List.of(SurveyPoint.builder().surveyId(2).points(1).build())
+                )
         );
     }
 
     @ParameterizedTest
     @MethodSource("fetchMembersToBeInvitedBySurveySource")
-    void fetchMembersToBeInvitedBySurvey(List<Participation> participations, List<Member> members, List<Survey> surveys,
+    void fetchMembersToBeInvitedBySurvey(List<Participation> participations, List<Member> members,
                                          Survey survey, List<Member> expectedResult) {
         //given
-        underTest = new DataFetchService(participations, members, surveys);
+        underTest = new DataFetchService(participations, members, List.of());
 
         //when
         List<Member> actualResult = underTest.fetchMembersToBeInvitedBySurvey(survey);
@@ -94,8 +166,35 @@ class DataFetchServiceTest {
     }
 
     private static Stream<Arguments> fetchMembersToBeInvitedBySurveySource() {
+        Member member1 = Member.builder().memberId(1).fullName("Malissa Arn").isActive(true).build();
+        Member member2 = Member.builder().memberId(2).fullName("Teri Villalobos").isActive(true).build();
+        Member member3 = Member.builder().memberId(3).fullName("Joella Verne").isActive(true).build();
+        Member member4 = Member.builder().memberId(4).fullName("Bettyann Kizer").isActive(false).build();
+        Survey survey = Survey.builder().surveyId(1).build();
+
         return Stream.of(
-                Arguments.of()
+                Arguments.of(
+                        List.of(),
+                        List.of(member1, member2, member4),
+                        survey,
+                        List.of(member1, member2)
+                ),
+                Arguments.of(
+                        List.of(Participation.builder().surveyId(1).memberId(1).status(NOT_ASKED).build(),
+                                Participation.builder().surveyId(1).memberId(4).status(NOT_ASKED).build(),
+                                Participation.builder().surveyId(1).memberId(3).status(COMPLETED).build(),
+                                Participation.builder().surveyId(2).memberId(2).status(NOT_ASKED).build()),
+                        List.of(member1, member2, member3, member4),
+                        survey,
+                        List.of(member1)
+                ),
+                Arguments.of(
+                        List.of(Participation.builder().surveyId(1).memberId(1).status(NOT_ASKED).build(),
+                                Participation.builder().surveyId(1).memberId(2).status(REJECTED).build()),
+                        List.of(member1, member2, member3, member4),
+                        survey,
+                        List.of(member1, member3)
+                )
         );
     }
 
@@ -114,8 +213,83 @@ class DataFetchServiceTest {
     }
 
     private static Stream<Arguments> fetchSurveysStatisticsSource() {
+        Survey survey1 = Survey.builder().surveyId(1).name("Survey1").build();
+        Survey survey2 = Survey.builder().surveyId(2).name("Survey2").build();
+
         return Stream.of(
-                Arguments.of()
+                Arguments.of(
+                        List.of(Participation.builder().surveyId(1).memberId(1).status(COMPLETED).length(5).build(),
+                                Participation.builder().surveyId(1).memberId(2).status(REJECTED).build(),
+                                Participation.builder().surveyId(2).memberId(1).status(FILTERED).build(),
+                                Participation.builder().surveyId(2).memberId(2).status(COMPLETED).length(3).build()),
+                        List.of(survey1, survey2),
+                        List.of(SurveyStatistics.builder()
+                                        .surveyId(1)
+                                        .surveyName("Survey1")
+                                        .numberOfCompletes(1)
+                                        .numberOfFilteredParticipants(0)
+                                        .numberOfRejectedParticipants(1)
+                                        .averageLength(5.0)
+                                        .build(),
+                                SurveyStatistics.builder()
+                                        .surveyId(2)
+                                        .surveyName("Survey2")
+                                        .numberOfCompletes(1)
+                                        .numberOfFilteredParticipants(1)
+                                        .numberOfRejectedParticipants(0)
+                                        .averageLength(3.0)
+                                        .build()
+                        )
+                ),
+                Arguments.of(
+                        List.of(Participation.builder().surveyId(1).memberId(1).status(COMPLETED).length(5).build(),
+                                Participation.builder().surveyId(1).memberId(2).status(COMPLETED).length(2).build()),
+                        List.of(survey1),
+                        List.of(SurveyStatistics.builder()
+                                .surveyId(1)
+                                .surveyName("Survey1")
+                                .numberOfCompletes(2)
+                                .numberOfFilteredParticipants(0)
+                                .numberOfRejectedParticipants(0)
+                                .averageLength(3.5)
+                                .build()
+                        )
+                ),
+                Arguments.of(
+                        List.of(),
+                        List.of(survey1),
+                        List.of(SurveyStatistics.builder()
+                                .surveyId(1)
+                                .surveyName("Survey1")
+                                .numberOfCompletes(0)
+                                .numberOfFilteredParticipants(0)
+                                .numberOfRejectedParticipants(0)
+                                .averageLength(0.0)
+                                .build()
+                        )
+                ),
+                Arguments.of(
+                        List.of(Participation.builder().surveyId(1).memberId(1).status(REJECTED).build(),
+                                Participation.builder().surveyId(1).memberId(2).status(REJECTED).build()),
+                        List.of(survey1, survey2),
+                        List.of(SurveyStatistics.builder()
+                                        .surveyId(1)
+                                        .surveyName("Survey1")
+                                        .numberOfCompletes(0)
+                                        .numberOfFilteredParticipants(0)
+                                        .numberOfRejectedParticipants(2)
+                                        .averageLength(0.0)
+                                        .build(),
+                                SurveyStatistics.builder()
+                                        .surveyId(2)
+                                        .surveyName("Survey2")
+                                        .numberOfCompletes(0)
+                                        .numberOfFilteredParticipants(0)
+                                        .numberOfRejectedParticipants(0)
+                                        .averageLength(0.0)
+                                        .build()
+                        )
+                )
         );
     }
 }
